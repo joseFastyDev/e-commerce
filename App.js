166419ad3e3e1aa194f5b9de54074b230d216a9b
joseFastyDev <jose.rangel@fastydev.com>
2022-10-26 18:03:44 -0500

    26oct2022

--- App.js
+++ App.js
@@ -1,18 +1,66 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
+import { Text, View, Button } from 'react-native';
 import { Provider as PaperProvider } from "react-native-paper"
+import jwtDecode from 'jwt-decode'; 
+import AppNavigation from './src/navigation/AppNavigation';
 import AuthScreen from "./src/screens/Auth";
+import AuthContext from "./src/context/AuthContext";
+import { setTokenApi, getTokenApi, removeTokenApi } from "./src/api/token";
 
 export default function App() {
   const [ auth, setAuth ] = useState(undefined);
-  return (
 
-    <PaperProvider>
-      { auth ? <Text>Zonda de usuarios</Text> : <AuthScreen />}
-    </PaperProvider>
+  useEffect(() => {
+    (async () => {
+      const token = await getTokenApi();
+      if(token) {
+        setAuth({
+          token,
+          idUser: jwtDecode(token).id
+        });
+      } else {
+        setAuth(null);
+      }
+    })()  
+  }, []);
+
+  const login = (user) => {
+    setTokenApi(user.jwt);
+    setAuth({
+      token: user.jwt,
+      idUser: user.user._id,
+    });
+  }
+
+  const logout = () => {
+    if(auth) {
+      removeTokenApi();
+      setAuth(null);
+    }
+  }
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      login,
+      logout,
+    }),
+    [auth]
+  );
+
+  if (auth === undefined) return null;
+
+  return (
+    <AuthContext.Provider
+      value={authData}
+    >
+      <PaperProvider>
+        { auth ? (
+          <AppNavigation />
+        ) : (
+          <AuthScreen />
+        )}
+      </PaperProvider>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  
-});
